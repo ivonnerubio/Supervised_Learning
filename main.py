0900d40affaf9ba42ec5823ec28365f0ed7b647c
Ivonne <irubio081@outlook.com>
2023-04-19 18:49:16 -0500

    adding data

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+st.title('Single Neuron Batch Linear Regression Model')
+st.selectbox("Dataset",["Steinmetz dataset (2019)"])
+st.text("References: Steinmetz, N. A., Zatka-Haas, P., Carandini, M., and Harris, K. D. (2019). Distributed coding of choice, action and engagement across the mouse brain. Nature, 576(7786): 266-273. doi: 10.1038/s41586-019-1787-x")
